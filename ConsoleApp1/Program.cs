df9ed369ae25813d4e5f11117961004c545769ec
losonczi.jozsef <losonczi.jozsef@BURG-1E05-10>
2023-09-12 08:54:43 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg az első számot:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add meg a második számot:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("A számok összege: {0}",c);
+            Console.ReadKey();
+        }
+    }
+}
